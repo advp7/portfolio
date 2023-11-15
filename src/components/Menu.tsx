05fa3b532b44793ec23514446d71c2db03adc8f2
Advaith <advaith1601@gmail.com>
2023-11-15 13:13:30 +0530

    changes to porfolio

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,6 +5,7 @@ import HomeIcon from "@mui/icons-material/Home";
 import PersonIcon from "@mui/icons-material/Person";
 import AppsIcon from "@mui/icons-material/Apps";
 import MailIcon from "@mui/icons-material/Mail";
+import LogoDevIcon from "@mui/icons-material/LogoDev";
 // react-scroll
 import { Link } from "react-scroll";
 
@@ -12,7 +13,7 @@ const Menu = () => {
   return (
     <div
       className="fixed bottom-0 sm:bottom-12 w-full sm:w-fit left-1/2 -translate-x-1/2 bg-accent
-  rounded-t-xl sm:rounded-full py-3 px-6 flex items-center justify-around sm:justify-center gap-12 z-30"
+  rounded-t-xl sm:rounded-full py-3 px-6 flex items-center justify-around sm:justify-center gap-10 z-30"
     >
       <Link to="home" smooth>
         <Tooltip title="Home" placement="top" arrow>
@@ -26,6 +27,14 @@ const Menu = () => {
         <Tooltip title="About" placement="top" arrow>
           <IconButton className="group">
             <PersonIcon className="text-white group-hover:text-secondary" />
+          </IconButton>
+        </Tooltip>
+      </Link>
+
+      <Link to="skills" smooth>
+        <Tooltip title="Skills" placement="top" arrow>
+          <IconButton className="group">
+            <LogoDevIcon className="text-white group-hover:text-secondary" />
           </IconButton>
         </Tooltip>
       </Link>
